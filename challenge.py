6082c27dabca9666742374c6f63e11d97e36493d
Christopher Agoro <chrisagoro@users.noreply.github.com>
2022-06-28 19:48:49 +0000

    join and split

---
+++ challenge.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+name =input("What is your name? ")
+day =input("what day of the week is it? ")
+print("Hello, " + name + "! Happy " + day + "!") 
+         #  ^
+         # just moved the comma inside of the "quotes" of the string!
