c0465f1aa972023e31da7c0e291e9c0816960149
Christopher Agoro <chrisagoro@users.noreply.github.com>
2022-06-29 19:49:23 +0000

    Learning about conditional if

---
+++ finaldictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+ironman= {"director": "Jon Favreau", "writer": "Art Marcum", "year": 2008}
+ironman["costume"] = "metaljacket"
+print(ironman.keys())
+
+choice= input("choose a key: ")
